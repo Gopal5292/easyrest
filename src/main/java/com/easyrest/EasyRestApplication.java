4bf2ea545f8f69e9b5601c567d944113a7e7dda1
Shibin Thomas <shibinthomas90@yahoo.in>
2018-07-31 19:36:22 +0530

    app

--- src/main/java/com/easyrest/EasyRestApplication.java
+++ src/main/java/com/easyrest/EasyRestApplication.java
@@ -1,5 +1,6 @@
 package com.easyrest;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -7,4 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class EasyRestApplication {
+
+    public static void main(String args[]){
+        SpringApplication.run(EasyRestApplication.class, args);
+    }
 }
