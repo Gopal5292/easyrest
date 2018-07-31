eb6c2957af1448f690e7f043b070212ad9c447db
Shibin Thomas <shibinthomas90@yahoo.in>
2018-07-31 22:06:14 +0530

    controller

---
+++ src/main/java/com/easyrest/controller/SpringController.java
@@ -0,0 +1,31 @@
+package com.easyrest.controller;
+
+import com.easyrest.ro.EasyRestRO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class SpringController {
+
+	private static final Logger logger = LoggerFactory.getLogger(SpringController.class);
+
+	@RequestMapping(value = "/create", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public String createEasyRest(@RequestBody EasyRestRO easyRest) {
+		return null;
+	}
+
+	@RequestMapping(value = "/requestmethod", method = RequestMethod.GET)
+	public List<String> getRequestMethods(){
+
+		return new ArrayList<>();
+	}
+
+
+
+}
